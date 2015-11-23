0840c6b88bfbe47f14554fdecc91c093de966efd
blackdartq <brody.prestwich666@gmail.com>
2015-11-23 13:36:32 -0700

    changed layout, added phone debug, added buttonColorChange method

--- app/src/main/java/com/example/blackdartq/cloudmath/MainActivity.java
+++ app/src/main/java/com/example/blackdartq/cloudmath/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.blackdartq.cloudmath;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -16,87 +17,12 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    RadioButton RadioButton1;
-    RadioButton RadioButton2;
-    RadioButton RadioButton3;
-    RadioGroup RadioGroup;
-
-    TextView Text1;
-    TextView Text2;
-    double first;
-    double second;
-
-    public void Amps() {
-
-
-        EditText TextBox1 = (EditText) findViewById(R.id.TextBox1);// creates the first box
-        EditText TextBox2 = (EditText) findViewById(R.id.TextBox2);// creates the second box
-        double FirstVal = 0;
-        double SecondVal = 0;
-
-        String FirstString = TextBox1.getText().toString();// gets the first string
-        String SecondString = TextBox2.getText().toString(); //gets the second string
-        if (!FirstString.isEmpty() || !SecondString.isEmpty())
-        {
-            // Handle error here
-        }
-        // makes them into doubles
-        FirstVal = Double.parseDouble(FirstString);
-        double First = FirstVal * 1;
-        SecondVal = Double.parseDouble(SecondString);
-        double Second = SecondVal * 1;
-        double TotalVar = First / Second * 1; // gets the amps
-        //
-        String Total = Double.toString(TotalVar); //makes TotalVar a string
-
-        TextView TotalBox = (TextView) findViewById(R.id.TotalBox);
-        TotalBox.setText(Total.substring(0, 4) + " Amps a battery needs.");
-
-    }
-
-    public void Watts() {
-
-
-        EditText TextBox1 = (EditText) findViewById(R.id.TextBox1);// creates the first box
-        EditText TextBox2 = (EditText) findViewById(R.id.TextBox2);// creates the second box
-
-        String FirstString = TextBox1.getText().toString();// gets the first string
-        String SecondString = TextBox2.getText().toString(); //gets the second string
-        // makes them into doubles
-        double FirstVal = Double.parseDouble(FirstString);
-        double SecondVal = Double.parseDouble(SecondString);
-        double TotalVar = FirstVal * SecondVal; // gets the amps
-        //
-        String Total = Double.toString(TotalVar); //makes TotalVar a string
-
-        TextView TotalBox = (TextView) findViewById(R.id.TotalBox);
-        TotalBox.setText("You have "+ Total.substring(0, 4) + " Watts of power");
-
-    }
-
-    public void Resist() {
-
-        EditText TextBox1 = (EditText) findViewById(R.id.TextBox1);// creates the first box
-        EditText TextBox2 = (EditText) findViewById(R.id.TextBox2);// creates the second box
-
-        String FirstString = TextBox1.getText().toString();// gets the first string
-        String SecondString = TextBox2.getText().toString(); //gets the second string
-        // makes them into doubles
-        double FirstVal = Double.parseDouble(FirstString);
-
-        double SecondVal = Double.parseDouble(SecondString);
-
-        double TotalVar = FirstVal / SecondVal; // gets the amps
-
-
-        String Total = Double.toString(TotalVar); //makes TotalVar a string
-
-        // makes the first side textbox Volts
-
-        TextView TotalBox = (TextView) findViewById(R.id.TotalBox);
-        TotalBox.setText("You have a " + Total.substring(0, 4) + " Ohm limit");
-
-    }
+    Button wattButton;
+    Button voltButton;
+    Button ampButton;
+    Button ohmButton;
+    TextView totalTextView;
+    int whichButtonsClicked;
 
 
 
@@ -107,86 +33,92 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        RadioButton1 = (RadioButton) findViewById(R.id.radioButton);
-        RadioButton2 = (RadioButton) findViewById(R.id.radioButton2);
-        RadioButton3 = (RadioButton) findViewById(R.id.radioButton3);
-        RadioGroup = (RadioGroup)findViewById(R.id.RadioGroup);
-        //-------------------------------------------------Amps---------------------------
-         RadioButton1.setOnClickListener(
-                 new RadioButton.OnClickListener() {
-                     public void onClick(View v) {
+        // ------------------------ Declaring section --------
 
-                         if (RadioButton1.isChecked()) {
-                             Text1 = (TextView) findViewById(R.id.Text1);
-                             Text2 = (TextView) findViewById(R.id.Text2);
-
-                             Text1.setText("Volts");
-                             Text2.setText("Ohms");
-
-                         }
-                         else {
-                         }
-                     }
-                 });
-        //-------------------------------------------------Ohm---------------------------
-        RadioButton2.setOnClickListener(
-                new RadioButton.OnClickListener() {
-                    public void onClick(View v) {
-
-                        if (RadioButton2.isChecked()) {
-                            Text1 = (TextView) findViewById(R.id.Text1);
-                            Text2 = (TextView) findViewById(R.id.Text2);
-
-                            Text1.setText("Volts");
-                            Text2.setText("Amps");
-
-                        }
-                    }
-                });
-        //-------------------------------------------------Watts---------------------------
-        RadioButton3.setOnClickListener(
-                new RadioButton.OnClickListener() {
-                    public void onClick(View v) {
-
-                        if (RadioButton3.isChecked()) {
-                            Text1 = (TextView) findViewById(R.id.Text1);
-                            Text2 = (TextView) findViewById(R.id.Text2);
-
-                            Text1.setText("Volts");
-                            Text2.setText("Amps");
-
-                        }
-                    }
-                });
-
-        //---------------------------Button-----------------------------------------
-
-        final Button TotalButton = (Button) findViewById(R.id.TotalButton);
-
-        TotalButton.setOnClickListener(
-                new Button.OnClickListener() {
-                    public void onClick(View v) {
-                        if (RadioButton1.isChecked()) {
-                            // Radio 1 selected
-                            Amps();
+        wattButton = (Button)findViewById(R.id.wattButton);
+        voltButton = (Button)findViewById(R.id.voltButton);
+        ohmButton = (Button)findViewById(R.id.ohmButton);
+        ampButton = (Button)findViewById(R.id.ampButton);
+        totalTextView = (TextView)findViewById(R.id.totalTextView);
 
 
-                        } else if (RadioButton2.isChecked()) {
-                            Resist();
+        //---------- Buttons-------------------------------------
 
-                        } else if (RadioButton3.isChecked()) {
-                            Watts();
+        ohmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                totalTextView.setText(" ");
+                whichButtonsClicked = 1;
+                changeButtonColor();
 
-                        } else {
-                            TotalButton.setText("please click ratio Button");
+            }
+        });
 
-                        }
-                    }
+        wattButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                totalTextView.setText(" ");
+                whichButtonsClicked = 2;
+                changeButtonColor();
 
-                }
+            }
+        });
 
-        );
+        ampButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                totalTextView.setText(" ");
+                whichButtonsClicked = 3;
+                changeButtonColor();
+            }
+        });
 
+
+        voltButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                totalTextView.setText(" ");
+                whichButtonsClicked = 4;
+                changeButtonColor();
+            }
+        });
+
+
+
+
+    }
+
+
+    public void changeButtonColor(){
+
+        // change everything back to normal
+        wattButton.setBackgroundColor(Color.CYAN);
+        wattButton.setTextColor(Color.BLACK);
+        ohmButton.setBackgroundColor(Color.CYAN);
+        ohmButton.setTextColor(Color.BLACK);
+        voltButton.setBackgroundColor(Color.CYAN);
+        voltButton.setTextColor(Color.BLACK);
+        ampButton.setBackgroundColor(Color.CYAN);
+        ampButton.setTextColor(Color.BLACK);
+
+        //sets the buttons colors
+        if (whichButtonsClicked == 1){
+            ohmButton.setBackgroundColor(Color.BLACK);
+            ohmButton.setTextColor(Color.CYAN);
+
+        }else if(whichButtonsClicked == 2){
+            wattButton.setBackgroundColor(Color.BLACK);
+            wattButton.setTextColor(Color.CYAN);
+
+        }else if(whichButtonsClicked == 3){
+            ampButton.setBackgroundColor(Color.BLACK);
+            ampButton.setTextColor(Color.CYAN);
+
+        }else if(whichButtonsClicked == 4){
+            voltButton.setBackgroundColor(Color.BLACK);
+            voltButton.setTextColor(Color.CYAN);
+
+        }
 
     }
 }
