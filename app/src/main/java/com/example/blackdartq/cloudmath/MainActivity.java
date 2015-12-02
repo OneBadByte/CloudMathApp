93a319b1aa48c47877c9aa8a744262a534fc422d
blackdartq <brody.prestwich666@gmail.com>
2015-12-02 13:46:15 -0700

    removed a few files, and app now changes orientation

--- app/src/main/java/com/example/blackdartq/cloudmath/MainActivity.java
+++ app/src/main/java/com/example/blackdartq/cloudmath/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.blackdartq.cloudmath;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -28,134 +29,16 @@ public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-
-        // ------------------------ Declaring section --------
-        wattButton = (Button)findViewById(R.id.wattButton);
-        voltButton = (Button)findViewById(R.id.voltButton);
-        ohmButton = (Button)findViewById(R.id.ohmButton);
-        ampButton = (Button)findViewById(R.id.ampButton);
-        infoButton = (Button)findViewById(R.id.infoButton);
-        totalTextView = (TextView)findViewById(R.id.totalTextView);
-
-
-
-        //---------- Buttons-------------------------------------
-
-        ohmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                totalTextView.setText(" ");
-                whichButtonsClicked = 1;
-                changeButtonColor();
-
-                OhmsFragment ohmsFragment = new OhmsFragment();
-                android.app.FragmentManager fm = getFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                ft.add(R.id.defaultFragmentlayout, ohmsFragment);
-                ft.commit();
-
-
-            }
-        });
-
-        wattButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                totalTextView.setText(" ");
-                whichButtonsClicked = 2;
-                changeButtonColor();
-
-                WattsFragment wattsFragment = new WattsFragment();
-                DefaultFragment defaultFragment = new DefaultFragment();
-                android.app.FragmentManager fm = getFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                ft.add(R.id.defaultFragmentlayout, wattsFragment);
-                ft.commit();
-
-            }
-        });
-
-        ampButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                totalTextView.setText(" ");
-                whichButtonsClicked = 3;
-                changeButtonColor();
-                AmpsFragment ampsFragment = new AmpsFragment();
-                android.app.FragmentManager fm = getFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                ft.add(R.id.defaultFragmentlayout, ampsFragment);
-                ft.commit();
-            }
-        });
-
-
-        voltButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                totalTextView.setText(" ");
-                whichButtonsClicked = 4;
-                changeButtonColor();
-                VoltsFragment voltsFragment = new VoltsFragment();
-                android.app.FragmentManager fm = getFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                ft.add(R.id.defaultFragmentlayout, voltsFragment);
-                ft.commit();
-
-            }
-        });
-
-        infoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getBaseContext(), InfoActivity.class);
-                startActivity(i);
-
-            }
-        });
-
-
-        //ft.commit();
-
-    }
-
-
-    public void changeButtonColor(){
-
-        // change everything back to normal
-        wattButton.setBackgroundColor(backgroundColor);
-        wattButton.setTextColor(Color.BLACK);
-        ohmButton.setBackgroundColor(backgroundColor);
-        ohmButton.setTextColor(Color.BLACK);
-        voltButton.setBackgroundColor(backgroundColor);
-        voltButton.setTextColor(Color.BLACK);
-        ampButton.setBackgroundColor(backgroundColor);
-        ampButton.setTextColor(Color.BLACK);
-
-        //sets the buttons colors
-        if (whichButtonsClicked == 1){
-            ohmButton.setBackgroundColor(Color.BLACK);
-            ohmButton.setTextColor(Color.CYAN);
-
-        }else if(whichButtonsClicked == 2){
-            wattButton.setBackgroundColor(Color.BLACK);
-            wattButton.setTextColor(Color.CYAN);
-
-        }else if(whichButtonsClicked == 3){
-            ampButton.setBackgroundColor(Color.BLACK);
-            ampButton.setTextColor(Color.CYAN);
-
-        }else if(whichButtonsClicked == 4){
-            voltButton.setBackgroundColor(Color.BLACK);
-            voltButton.setTextColor(Color.CYAN);
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.portrait_layout);
+        } else {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.landscape_layout);
 
         }
-
     }
-
 
 }
 
